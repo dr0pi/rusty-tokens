9492de106b829195d41c3df9264cf4d947ef3997
cdouven <christian.douven@zalando.de>
2016-09-19 14:17:11 +0200

    #1 removed signature from parsing

--- src/jwt/mod.rs
+++ src/jwt/mod.rs
@@ -131,7 +131,7 @@ impl JsonWebToken {
 impl FromStr for JsonWebToken {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (header_str, payload_str, _signature_str) = try!{decode_segments(s)};
+        let (header_str, payload_str) = try!{decode_data_segments(s)};
         let header = try!{parse_json_str_to_json_map(&header_str)};
         let payload = try!{parse_json_str_to_json_map(&payload_str)};
         Ok(JsonWebToken {
@@ -155,28 +155,32 @@ fn parse_json_str_to_json_map(json_str: &str) -> Result<HashMap<String, Json>, S
     }
 }
 
-fn decode_segments(complete: &str) -> Result<(String, String, String), String> {
-    let (header, payload, signature) = try!{split_segments(complete).map_err(|x| x.to_owned())};
-    let decoded_header = try!{decode_base_64(header)};
-    let decoded_payload = try!{decode_base_64(payload)};
-    let decoded_signature = try!{decode_base_64(signature)};
-    Ok((decoded_header, decoded_payload, decoded_signature))
+fn decode_data_segments(complete: &str) -> Result<(String, String), String> {
+    let (header, payload) = try!{extract_data_segments(complete).map_err(|x| x.to_owned())};
+    let decoded_header = try!{decode_base_64_string(header)};
+    let decoded_payload = try!{decode_base_64_string(payload)};
+    Ok((decoded_header, decoded_payload))
 }
 
-fn split_segments(complete: &str) -> Result<(&str, &str, &str), &'static str> {
+fn extract_data_segments(complete: &str) -> Result<(&str, &str), &'static str> {
     let parts: Vec<&str> = complete.split('.').collect();
     if parts.len() == 3 {
-        Ok((parts[0], parts[1], parts[2]))
+        Ok((parts[0], parts[1]))
     } else {
         Err("The given String must split to 3 parts segmented by a dot each.")
     }
 }
 
-fn decode_base_64(what: &str) -> Result<String, String> {
-    let bytes =
-        try!{what.from_base64().map_err(|err| format!("Not a base64 encoded String: {}", err))};
+fn decode_base_64_bytes(what: &str) -> Result<Vec<u8>, String> {
+    let bytes: Vec<u8> =
+        try!{what.from_base64().map_err(|err| format!("No base64 encoded bytes: {}", err))};
+    Ok(bytes)
+}
+
+fn decode_base_64_string(what: &str) -> Result<String, String> {
+    let bytes: Vec<u8> = try!{decode_base_64_bytes(what)};
     let string =
-        try!{String::from_utf8(bytes).map_err(|err| format!("Not a valid UTF-8 String: {}", err))};
+        try!{String::from_utf8(bytes).map_err(|err| format!("basde64 bytes are not a valid UTF-8 String: {}", err))};
     Ok(string)
 }
 
@@ -189,44 +193,41 @@ mod test {
                                         KmDsVB09RAOYwT0Y6E9tdQpg0rAPd8SExYhcZ9tXEO6y9AWX4wBylnmNHVoetWu7MwoexWkaKdpKk09IodMVug";
 
     const sample_header_json: &'static str = "{\"kid\":\"testkey-es256\",\"alg\":\"ES256\"}";
-    const sample_payload_json: &'static str =
-        "{\"sub\":\"test2\",\"scope\":[\"cn\"],\"iss\":\"B\",\"realm\":\"/services\",\"exp\":\
-         1457319814,\"iat\":1457291014}";
+    const sample_payload_json: &'static str = "{\"sub\":\"test2\",\"scope\":[\"cn\"],\"iss\":\
+                                               \"B\",\"realm\":\"/services\",\"exp\":1457319814,\
+                                               \"iat\":1457291014}";
 
     #[test]
     fn must_decode_base_64_header_to_a_string() {
         let sample = "eyJraWQiOiJ0ZXN0a2V5LWVzMjU2IiwiYWxnIjoiRVMyNTYifQ";
-        let result = jwt::decode_base_64(sample).unwrap();
+        let result = jwt::decode_base_64_string(sample).unwrap();
         assert_eq!(sample_header_json, result);
     }
 
     #[test]
     fn must_decode_base_64_payload_to_a_string() {
         let sample = "eyJzdWIiOiJ0ZXN0MiIsInNjb3BlIjpbImNuIl0sImlzcyI6IkIiLCJyZWFsbSI6Ii9zZXJ2aWNlcyIsImV4cCI6MTQ1NzMxOTgxNCwiaWF0IjoxNDU3MjkxMDE0fQ";
-        let result = jwt::decode_base_64(sample).unwrap();
+        let result = jwt::decode_base_64_string(sample).unwrap();
         assert_eq!(sample_payload_json, result);
     }
 
     #[test]
-    fn split_segments_must_work() {
+    fn split_data_segments_must_work() {
         let sample = sample_token;
         let expected = ("eyJraWQiOiJ0ZXN0a2V5LWVzMjU2IiwiYWxnIjoiRVMyNTYifQ",
-                        "eyJzdWIiOiJ0ZXN0MiIsInNjb3BlIjpbImNuIl0sImlzcyI6IkIiLCJyZWFsbSI6Ii9zZXJ2aWNlcyIsImV4cCI6MTQ1NzMxOTgxNCwiaWF0IjoxNDU3MjkxMDE0fQ",
-                        "KmDsVB09RAOYwT0Y6E9tdQpg0rAPd8SExYhcZ9tXEO6y9AWX4wBylnmNHVoetWu7MwoexWkaKdpKk09IodMVug");
+                        "eyJzdWIiOiJ0ZXN0MiIsInNjb3BlIjpbImNuIl0sImlzcyI6IkIiLCJyZWFsbSI6Ii9zZXJ2aWNlcyIsImV4cCI6MTQ1NzMxOTgxNCwiaWF0IjoxNDU3MjkxMDE0fQ");
 
-        let result = jwt::split_segments(sample).unwrap();
+        let result = jwt::extract_data_segments(sample).unwrap();
 
         assert_eq!(expected, result);
     }
 
     #[test]
-    fn decode_segments_must_work() {
+    fn decode_data_segments_must_work() {
         let sample = sample_token;
-        let expected = (sample_header_json.to_owned(),
-                        sample_payload_json.to_owned(),
-                        "KmDsVB09RAOYwT0Y6E9tdQpg0rAPd8SExYhcZ9tXEO6y9AWX4wBylnmNHVoetWu7MwoexWkaKdpKk09IodMVug".to_owned());
+        let expected = (sample_header_json.to_owned(), sample_payload_json.to_owned());
 
-        let result = jwt::decode_segments(sample).unwrap();
+        let result = jwt::decode_data_segments(sample).unwrap();
 
         assert_eq!(expected, result);
     }
